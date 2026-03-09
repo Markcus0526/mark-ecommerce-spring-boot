0b79f93f7e58277d1a01473cdf367da40f838a4e
markcus <markcus0526@proton.me>
2026-03-09 07:38:06 +0100

    Update shipping-service

--- shipping-service/src/main/resources/db/migration/V1__create_order_items_table.sql
+++ shipping-service/src/main/resources/db/migration/V1__create_order_items_table.sql
@@ -1,10 +1,10 @@
 
 CREATE TABLE order_items (
-	product_id INT(11) NOT NULL,
-	order_id INT(11) NOT NULL,
-	ordered_quantity INT(11),
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP,
+	product_id INT NOT NULL,
+	order_id INT NOT NULL,
+	ordered_quantity INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NULL,
 	PRIMARY KEY (product_id, order_id)
 );
 
